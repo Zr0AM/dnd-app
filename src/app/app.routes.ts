120418dc226f6c3ca11859533e0b8816c6a62b42
Zr0AM <103156517+Zr0AM@users.noreply.github.com>
2026-03-03 17:01:39 -0600

    Fixed tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from './home-component/home-component';
 import {LootSplitter} from './loot-splitter/loot-splitter';
 
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent, data: {name: 'home'}},
-  {path: 'loot-splitter', component: LootSplitter, data: {name: 'Loot splitter'}},
+  {path: 'home', component: HomeComponent, title: 'DND', data: {name: 'home'}},
+  {path: 'loot-splitter', component: LootSplitter, title: 'DND', data: {name: 'Loot splitter'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
