29a3f08958da2bfdaf9ebe981dbb65bf94c88726
Zr0AM <103156517+Zr0AM@users.noreply.github.com>
2026-03-29 13:13:57 -0500

    Fixed local config loading

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,14 @@
-import { envConfig } from './env-config.local';
-
 export const environment = {
   production: false,
-  envConfig: envConfig
+  envConfig: {} as any
 };
+
+// Attempt to load it asynchronously
+try {
+  // @ts-ignore
+  import('./env-config.local').then(localEnv => {
+    environment.envConfig = localEnv.envConfig;
+  }).catch(() => { /* File not found, ignore */
+  });
+} catch (e) {
+}
